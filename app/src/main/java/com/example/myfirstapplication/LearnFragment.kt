197422eefa8654f5f3dcc5a146cdcc41bf4c465c
Aiden Gu <aiden.gu01@gmail.com>
2020-09-22 18:00:13 +1000

    Learn navigation

--- app/src/main/java/com/example/myfirstapplication/LearnFragment.kt
+++ app/src/main/java/com/example/myfirstapplication/LearnFragment.kt
@@ -11,5 +11,7 @@ class LearnFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_learn, container, false)
+
     }
+
 }
