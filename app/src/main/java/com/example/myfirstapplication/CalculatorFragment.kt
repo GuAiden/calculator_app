83c157694d994526462a782ec9cdf3c130227eb3
Aiden Gu <aiden.gu01@gmail.com>
2020-11-30 02:23:44 +1100

    Format colors

--- app/src/main/java/com/example/myfirstapplication/CalculatorFragment.kt
+++ app/src/main/java/com/example/myfirstapplication/CalculatorFragment.kt
@@ -28,7 +28,7 @@ class CalculatorFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.calculator, container, false)
     }
 
 
